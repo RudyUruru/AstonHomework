09df8b9f71ee5c5f71bc362a937a0a008bccd524
SP <student-programmer@mail.ru>
2024-09-02 23:18:04 +0300

    Node added

---
+++ src/main/java/org/example/collection/LinkedList.java
@@ -0,0 +1,17 @@
+package org.example.collection;
+
+public class LinkedList<E> {
+
+    private static class Node<E> {
+        E item;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(Node<E> prev, E item, Node<E> next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+}
