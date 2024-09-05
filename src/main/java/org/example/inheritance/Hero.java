ed58e0bc86072a9e7f6ff69228bc34092acba91f
Buba <b01bc@mail.ru>
2024-09-06 00:59:01 +0300

    Hero class added

---
+++ src/main/java/org/example/inheritance/Hero.java
@@ -0,0 +1,19 @@
+package org.example.inheritance;
+
+public class Hero {
+
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attackEnemy(Hero target) {
+        System.out.println(name + " attack " + target.name);
+    }
+
+}
