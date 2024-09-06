87c0385df798aaba74d19e6ac5ea386067c5833d
Buba <b01bc@mail.ru>
2024-09-07 01:40:50 +0300

    TrainingGround deprecated

--- src/main/java/org/example/inheritance/TrainingGround.java
+++
@@ -1,9 +0,0 @@
-package org.example.inheritance;
-
-public class TrainingGround {
-    public static void main(String[] args) {
-        Hero hero = new Hero("Hasbik");
-        Hero target = new Hero("Nefor");
-        hero.attackEnemy(target);
-    }
-}
